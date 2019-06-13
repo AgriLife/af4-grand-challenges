9f2f6aaaed6b06bd051a3012be3623cd55d8cb96
Zachary Watkins <watkinza@gmail.com>
2019-06-13 15:59:54 -0500

    initial commit

---
+++ af4-grand-challenges.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Grand Challenges for the College of Agriculture and Life Sciences
+ *
+ * @package      Grand Challenges
+ * @author       Zachary Watkins
+ * @copyright    2019 Texas A&M AgriLife Communications
+ * @license      GPL-2.0+
+ *
+ * @wordpress-plugin
+ * Plugin Name:  Grand Challenges
+ * Plugin URI:   https://github.com/AgriLife/af4-grand-challenges
+ * Description:  Grand Challenges faculty search form for AgriFlex4.
+ * Version:      0.1.0
+ * Author:       Zachary Watkins
+ * Author URI:   https://github.com/ZachWatkins
+ * Author Email: zachary.watkins@ag.tamu.edu
+ * Text Domain:  af4-grand-challenges
+ * License:      GPL-2.0+
+ * License URI:  http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+/* Define some useful constants */
+define( 'AF4_GC_DIRNAME', 'af4-grand-challenges' );
+define( 'AF4_GC_DIR_PATH', plugin_dir_path( __FILE__ ) );
+define( 'AF4_GC_DIR_FILE', __FILE__ );
+define( 'AF4_GC_DIR_URL', plugin_dir_url( __FILE__ ) );
+define( 'AF4_GC_TEXTDOMAIN', 'af4-grand-challenges' );
+define( 'AF4_GC_TEMPLATE_PATH', AF4_GC_DIR_PATH . 'templates' );
+
+/* Code for plugins */
+register_deactivation_hook( __FILE__, 'flush_rewrite_rules' );
+register_activation_hook( __FILE__, 'af4_gc_activation' );
+
+/**
+ * Helper option flag to indicate rewrite rules need flushing
+ *
+ * @since 0.1.0
+ * @return void
+ */
+function af4_gc_activation() {
+	$theme = wp_get_theme();
+	if ( 'AgriFlex4' !== $theme->name ) {
+		$error = sprintf(
+			/* translators: %s: URL for plugins dashboard page */
+			__(
+				'Plugin NOT activated: The <strong>Grand Challenges Plugin</strong> needs the <strong>AgriFlex4 Theme</strong> to be installed and activated first. <a href="%s">Back to plugins page</a>',
+				'af4-college'
+			),
+			get_admin_url( null, '/plugins.php' )
+		);
+		wp_die( wp_kses_post( $error ) );
+	}
+
+	if ( ! get_option( 'af4_gc_flush_rewrite_rules_flag' ) ) {
+		add_option( 'af4_gc_flush_rewrite_rules_flag', true );
+	}
+}
+
+/**
+ * The core plugin class that is used to initialize the plugin
+ */
+require ALAF4_DIR_PATH . 'src/class-grandchallenges.php';
+
+/* Autoload all classes */
+spl_autoload_register( 'GrandChallenges::autoload' );
+GrandChallenges::get_instance();
---
+++ readme.md
@@ -0,0 +1,35 @@
+# Grand Challenges (AgriFlex4)
+
+## WordPress Requirements
+
+1. Genesis theme
+2. AgriFlex4 theme: [Download the latest release](https://github.com/agrilife/agriflex4/releases/latest)
+3. AgriLife People API plugin: [Download the latest release](https://github.com/AgriLife/agrilife-people-api/releases/latest)
+4. PHP 5.6+, tested with PHP 7.2
+
+## Installation
+
+1. [Download the latest release](https://github.com/agrilife/af4-grand-challenges/releases/latest)
+2. Upload the plugin to your site
+
+## Features
+
+## Development Installation
+
+1. Copy this repo to the desired location.
+2. In your terminal, navigate to the plugin location 'cd /path/to/the/plugin'
+3. Run "npm start" to configure your local copy of the repo, install dependencies, and build files
+
+## Development Notes
+
+When you stage changes to this repository and initiate a commit, they must pass PHP coding standards checks before they will complete the commit step. Release tasks can only be used by the repository's owners.
+
+## Development Tasks
+
+1. Run "npm run checkwp" to check PHP files against WordPress coding standards.
+
+## Development Requirements
+
+* Node: http://nodejs.org/
+* NPM: https://npmjs.org/
+
